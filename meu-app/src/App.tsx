1bbc936af96713d9c338a9c589069877a47610f7
AllysonAbreu <alllyson.cz@gmail.com>
2023-07-03 10:43:46 -0300

    primeiro componente dialog

---
+++ meu-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DialogDemo from './components/button_dialog/dialog.component';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Meu App</h1>
+      <DialogDemo />
+    </div>
+  );
+};
+
+export default App;
